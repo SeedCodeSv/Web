2613d559d0e6aca13bb55e22614bdb93cf8b7147
Marvin Carbajal <marvinhcarbajal09@gmail.com>
2023-07-27 08:21:51 -0600

    change in contact
    change in view contact

--- src/components/home/videoComponent.vue
+++ src/components/home/videoComponent.vue
@@ -1,21 +1,4 @@
 <template>
-  <!-- <div class="relative">
-    <video autoplay muted loop class="w-full h-auto object-cover">
-      <source
-        src="https://v4.cdnpk.net/videvo_files/video/free/video0467/large_watermarked/_import_615a995eea3f30.45528318_FPpreview.mp4"
-        type="video/mp4"
-      />
-    </video>
-    <div
-      class="absolute top-0 left-0 w-full text-opacity-80 h-full flex justify-center items-center text-center text-white font-black text-4xl md:text-5xl lg:text-6xl"
-    >
-      <div>
-        Nutriendo ideas y cultivando<span class="block"
-          >innovación SeedCode</span
-        >
-      </div>
-    </div>
-  </div> -->
   <div class="relative">
   <video autoplay muted loop class="w-full h-auto object-cover">
     <source
